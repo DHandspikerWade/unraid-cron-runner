94dd2e8f4d66a911e87da1238bd70ebd63b5af3e
Devin Handspiker-Wade <Devin@handspiker-wade.com>
2022-08-18 23:34:37 -0300

    Include $PATH

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-php /crontab_builder.php /cron_scripts/ > /etc/cron.d/crontab
+echo "SHELL=/bin/sh" > /etc/cron.d/crontab
+echo "PATH=$PATH" >> /etc/cron.d/crontab
+echo "" >> /etc/cron.d/crontab
+php /crontab_builder.php /cron_scripts/ >> /etc/cron.d/crontab
 chmod 0644 /etc/cron.d/crontab && /usr/bin/crontab /etc/cron.d/crontab
 
 exec cron -f 
