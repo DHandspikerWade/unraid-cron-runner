08455b741756d2fa963c653b4fa893c5002f9bd5
Devin Handspiker-Wade <devin@handspiker-wade.com>
2022-12-10 00:51:46 -0400

    Include DOCKER_HOST envronment variable

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 echo "SHELL=/bin/sh" > /etc/cron.d/crontab
 echo "PATH=$PATH" >> /etc/cron.d/crontab
+if [ -n "$DOCKER_HOST" ]; then
+    # include Docker connection if set
+    echo "DOCKER_HOST=$DOCKER_HOST" >> /etc/cron.d/crontab
+fi
 echo "" >> /etc/cron.d/crontab
+
 php /crontab_builder.php /cron_scripts/ >> /etc/cron.d/crontab
 chmod 0644 /etc/cron.d/crontab && /usr/bin/crontab /etc/cron.d/crontab
 
